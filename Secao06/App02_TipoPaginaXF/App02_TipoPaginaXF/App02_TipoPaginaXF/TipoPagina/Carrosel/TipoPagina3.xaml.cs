d884342dc40591dde57d69f644d86c136c80ce46
Dalton Romano <daltondrums@live.com>
2019-05-26 14:13:56 -0300

    aaa

---
+++ Secao06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Carrosel/TipoPagina3.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Carrosel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TipoPagina3 : ContentPage
+	{
+		public TipoPagina3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarPagina(object sender,EventArgs args)
+        {
+
+            //App.Current.MainPage = new NavigationPage(new Navigation.Pagina1())
+            //{
+            //    BarBackgroundColor = Color.Blue,
+            //    BarTextColor = Color.White
+            //};
+
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+	}
+}
